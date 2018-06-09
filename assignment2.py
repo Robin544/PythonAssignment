65a47d4a90d36b62132249d7291198b4af1042f3
Robin544 <robinsinghkamboj@gmail.com>
2018-06-09 07:41:13 +0530

    My 2nd assignment commit

---
+++ assignment2.py
@@ -0,0 +1,28 @@
+# Print anything you want on screen
+print("Acadview is the best teaching platform")
+
+# Join two strings using '+'
+print("Robin" + " " + "Singh")
+
+# Take the input of 3 variables x,y & z, print their values on screen
+x = int(input("enter the value of x: "))
+y = int(input("enter the value of y: "))
+z = int(input("enter the value of z: "))
+print("The value of x: ", x, "The value of y: ", y, "The value of z: ", z, sep="\n")
+
+# print "let's get started" on the screen.
+print("Let\'s get started")
+
+# print the following values using placeholders
+s = "Acadview"
+course = "Python"
+lang = "Language"
+fees = 5000
+print(("%s course for %s %s is of %dRs")%(s, course, lang, fees))
+
+# Some interesting exercise
+name = "Tony Stark"
+salary = 1000000
+print(('%s  %d')%(name, salary))
+
+print("Done")
